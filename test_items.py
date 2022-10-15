a2860705533d3f4f6a4ed6b1518a45b460c365d1
lilitstepanyan19 <lil-lid@mail.ru>
2022-10-15 10:40:25 +0400

    update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,16 +1,17 @@
+import time
+from cgitb import text
+
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from cgitb import text
-import time
 
 link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 def test_add_to_cart_button(browser):
     browser.get(link)
-    time.sleep(30)
-    btn = browser.find_element(By.CLASS_NAME, 'btn-add-to-basket')
-    assert btn, 'Add to cart button missing'
+    time.sleep(5)
+    btn = browser.find_element(By.CLASS_NAME, 'btdn-add-to-basket')
+    assert btn, NoSuchElementException
     
 if __name__ == '__main__':
     pytest.main()
